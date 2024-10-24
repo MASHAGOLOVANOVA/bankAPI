3ebcbd458cad291f46d7cd41679e255c447056ca
MASHAGOLOVANOVA <mmgolovanova@mail.ru>
2024-10-24 08:37:04 +0400

    spring initializr project

---
+++ src/test/java/gd/testtask/golovanova/bankAPI/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gd.testtask.golovanova.bankAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
