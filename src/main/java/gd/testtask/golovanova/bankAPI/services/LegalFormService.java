f77ce9acef312673e4c01c0c139eb416f3137d68
MASHAGOLOVANOVA <mmgolovanova@mail.ru>
2024-10-24 23:12:03 +0400

    services: LegalFormService enriched

--- src/main/java/gd/testtask/golovanova/bankAPI/services/LegalFormService.java
+++ src/main/java/gd/testtask/golovanova/bankAPI/services/LegalFormService.java
@@ -1,4 +1,43 @@
 package gd.testtask.golovanova.bankAPI.services;
 
+import gd.testtask.golovanova.bankAPI.dto.LegalFormDTO;
+import gd.testtask.golovanova.bankAPI.models.LegalForm;
+import gd.testtask.golovanova.bankAPI.repositories.LegalFormRepository;
+import gd.testtask.golovanova.bankAPI.util.LegalFormNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
 public class LegalFormService {
+
+    private final LegalFormRepository legalFormRepository;
+    private final ModelMapper modelMapper;
+
+    public LegalFormService(LegalFormRepository legalFormRepository) {
+        this.legalFormRepository = legalFormRepository;
+        this.modelMapper = new ModelMapper();
+    }
+
+    public List<LegalForm> findAll() {
+        return legalFormRepository.findAll();
+    }
+
+    public LegalFormDTO findOne(int id) {
+        Optional<LegalForm> bank = legalFormRepository.findById(id);
+        return convertToLegalFormDTO(bank.orElseThrow(LegalFormNotFoundException::new));
+    }
+
+    private LegalForm convertToLegalForm(LegalFormDTO legalFormDTO) {
+        return modelMapper.map(legalFormDTO, LegalForm.class);
+    }
+
+    public LegalFormDTO convertToLegalFormDTO(LegalForm legalForm) {
+        return modelMapper.map(legalForm, LegalFormDTO.class);
+    }
+
 }
